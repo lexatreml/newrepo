8fb375ad467374f7d4c8a1354290d60f63030241
admin <admin>
2022-05-03 10:44:45 -0600

    Saved recipe: DKU_TUTORIAL_FIRSTPLUGIN_1.compute_output_corr

--- recipes/compute_output_corr.py
+++ recipes/compute_output_corr.py
@@ -4,18 +4,18 @@ import dataiku
 import pandas as pd, numpy as np
 from dataiku import pandasutils as pdu
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Read recipe inputs
 wine_correlation = dataiku.Dataset("wine_correlation")
 wine_correlation_df = wine_correlation.get_dataframe()
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+#assign new dataframe to output
+output_corr_df = df.copy()
 
-# Compute recipe outputs from inputs
-# TODO: Replace this part by your actual code that computes the output, as a Pandas dataframe
-# NB: DSS also supports other kinds of APIs for reading and writing data. Please see doc.
-
-output_corr_df = wine_correlation_df # For this sample code, simply copy input to output
-
-
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Write recipe outputs
 output_corr = dataiku.Dataset("output_corr")
 output_corr.write_with_schema(output_corr_df)
+
+
